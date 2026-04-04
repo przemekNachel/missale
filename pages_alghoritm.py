3e71f106dc7039250f3504f5737d0b1696affe9a
przemekNachel <przemeknachel@gmail.com>
2026-04-05 00:53:57 +0200

    jgh

---
+++ pages_alghoritm.py
@@ -0,0 +1,43 @@
+import os
+from mergea5toa4 import merge_fixed_rotation
+
+def prepare_papers(number_of_pages):
+    pages = number_of_pages
+    number_of_papers = int(pages / 4)
+    if pages % 4 != 0:
+        number_of_papers += 1
+    papers = []
+    empty_pages = (number_of_papers * 4) - pages
+    b = 1
+    e = pages + empty_pages
+    for page in range(number_of_papers):
+        current_paper = {"BL": b, "L": b + 1 , "R": e -1, "BR": e}
+        papers.append(current_paper)
+        b += 2
+        e -= 2
+    if empty_pages == 1:
+        papers[0]["BR"] = None
+    elif empty_pages == 2:
+        papers[0]["BR"] = None
+        papers[0]["R"] = None
+    elif empty_pages == 3:
+        papers[0]["BR"] = None
+        papers[0]["R"] = None
+        if pages == 1:
+            papers[0]["L"] = None
+        else:
+            papers[1]["BR"] = None
+    return papers
+
+number_of_pages = 1
+while True:
+    if not os.path.exists("{}.html".format(number_of_pages)):
+        number_of_pages -= 1
+        break
+    number_of_pages += 1
+print(prepare_papers(number_of_pages))
+merged = 1
+for paper in prepare_papers(number_of_pages):
+    merge_fixed_rotation(f"{paper["BL"]}.pdf", f"{paper["BR"]}.pdf", f"merged_{merged}.pdf")
+    merge_fixed_rotation(f"{paper["L"]}.pdf", f"{paper["R"]}.pdf", f"merged_{merged + 1}.pdf")
+    merged += 2
